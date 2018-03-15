2f6021818c87588fb7d94ede9d76c1d7a721c102
Arife Kubra <arfekubra@gmail.com>
2018-03-15 14:53:02 +0300

    hw1 created

---
+++ 2018_HW1_CENG427/app/src/main/java/com/example/arife/a2018_hw1_ceng427/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.arife.a2018_hw1_ceng427;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
