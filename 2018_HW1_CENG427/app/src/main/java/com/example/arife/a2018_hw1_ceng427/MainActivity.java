6cf7ca56df21e6e0730117077f1def91551c3c7b
Arife Kubra <arfekubra@gmail.com>
2018-03-26 21:51:05 +0300

    edit and removed added

--- 2018_HW1_CENG427/app/src/main/java/com/example/arife/a2018_hw1_ceng427/MainActivity.java
+++ 2018_HW1_CENG427/app/src/main/java/com/example/arife/a2018_hw1_ceng427/MainActivity.java
@@ -1,57 +1,74 @@
 package com.example.arife.a2018_hw1_ceng427;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Calendar;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Date;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editText;
-    private Button addButton;
     private ListView listView;
-    private ArrayAdapter<String> arrayAdapter;
     private ArrayList<String> arrayList;
-    private Scanner scanner;
-
+    private static String TAG = "ListActivity";
+    private MyAdapter myAdapter;
+    private FirebaseDatabase fb;
+    private DatabaseReference dbReferance;
+    private Date currentTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        editText = findViewById(R.id.addListText);
         listView = findViewById(R.id.todoList);
         arrayList = new ArrayList<>();
-        scanner = new Scanner(getResources().openRawResource(R.raw.list));
-        while(scanner.hasNext()){
-            arrayList.add(scanner.nextLine());
-        }
 
-        MyAdapter myAdapter = new MyAdapter(this,arrayList);
+        fb = FirebaseDatabase.getInstance();
+        dbReferance = fb.getReference("ToDoList");
+
+
+        myAdapter = new MyAdapter(this, arrayList, dbReferance);
         listView.setAdapter(myAdapter);
+        readData();
+    }
+    public void readData(){
 
+        dbReferance.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                arrayList.clear();
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                    String item = ds.getValue(String.class);
+                    arrayList.add(item);
+                }
+                myAdapter.notifyDataSetChanged();
+            }
 
-
-
-
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                    Log.e(TAG, "error to read database", databaseError.toException());
+            }
+        });
+    }
+    public void addNewItem(View v){
+        currentTime = Calendar.getInstance().getTime();
+        String newItem= editText.getText().toString();
+        dbReferance.child(currentTime.toString()).setValue(newItem);
     }
 
-    public void addNewThings(){
-
-        scanner = new Scanner(getResources().openRawResource(R.raw.list));
-       //burada sonuna ekleme yapıyoruz
-    }
-
-
-    public void editText(View view) {
-
-        Toast.makeText(getApplicationContext(),"edit",Toast.LENGTH_LONG).show();
-    }
 }
--- 2018_HW1_CENG427/app/src/main/java/com/example/arife/a2018_hw1_ceng427/MyAdapter.java
+++ 2018_HW1_CENG427/app/src/main/java/com/example/arife/a2018_hw1_ceng427/MyAdapter.java
@@ -1,16 +1,24 @@
 package com.example.arife.a2018_hw1_ceng427;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,27 +28,121 @@ import java.util.List;
  */
 
 public class MyAdapter extends ArrayAdapter<String>{
-    private LayoutInflater layoutInflater;
+
     private TextView listItemText;
     private ImageButton editButton;
     private ImageButton removeButton;
+    private DatabaseReference dbReferance;
+    private ArrayList<String> myList;
+    private String item;
+    private static String TAG ="AdapterActivity";
 
-    public MyAdapter(@NonNull Context context, @NonNull List<String> objects) {
+
+    public MyAdapter(@NonNull Context context, @NonNull List<String> objects,DatabaseReference dbReferance) {
         super(context,0, objects);
+        this.dbReferance = dbReferance;
+        myList = (ArrayList<String>) objects;
     }
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         
-        String item = getItem(position);
+        item = getItem(position);
 
-        convertView = layoutInflater.from(this.getContext()).inflate(R.layout.list_item,null);
+        convertView = LayoutInflater.from(this.getContext()).inflate(R.layout.list_item,null);
         listItemText = convertView.findViewById(R.id.todoItemText);
         editButton = convertView.findViewById(R.id.editButton);
         removeButton = convertView.findViewById(R.id.removeButton);
+        final int pos = position;
+
+        removeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                remove(myList.get(pos));
+            }
+        });
+
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                update(myList.get(pos), view);
+
+            }
+        });
+
 
         listItemText.setText(item);
         return convertView;
     }
+
+
+    @Override
+    public void remove(@Nullable final String object) {
+
+        dbReferance.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot ds: dataSnapshot.getChildren()){
+                        String item = ds.getValue(String.class);
+                        if(item.equals(object) ){
+                            dbReferance.child(ds.getKey()).removeValue();
+                        }
+                    }
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        notifyDataSetChanged();
+    }
+
+    public void update(final String object, final View view){
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(view.getContext());
+        View v = LayoutInflater.from(view.getContext()).inflate(R.layout.edit_item,null);
+
+        alertDialog.setView(v);
+        final EditText editText = v.findViewById(R.id.editText);
+
+        alertDialog.setTitle(object);
+        alertDialog.setMessage("Change to: ");
+
+        alertDialog.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if(!editText.getText().toString().equals("")){
+                    dbReferance.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            for(DataSnapshot ds:dataSnapshot.getChildren()){
+                                String item = ds.getValue(String.class);
+                                if(item.equals(object)){
+                                    dbReferance.child(ds.getKey()).setValue(editText.getText().toString());
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                        }
+                    });
+                }
+                else
+                    Toast.makeText(view.getContext(),"Can not be empty",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        AlertDialog a = alertDialog.create();
+        a.show();
+    }
+
+
 }
